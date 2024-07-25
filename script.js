e6f45315e0975db257ab180db42ea40f2faaff9c
raghuls23 <raghul.workfolio@gmail.com>
2024-07-26 01:24:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,23 +9,28 @@ console.log(magicNumber);
 var easyBtn = document.getElementById("easy");
 var hardBtn = document.getElementById("hard");
 var result = document.getElementById("result");
-var attempt = 0;
+var attempt = 5;
 const canvas = document.querySelector('#confetti');
 const jsConfetti = new JSConfetti();
-easyBtn.addEventListener("click", function() {
-    attempt = 5;
-    result.textContent = `You have ${attempt} chances left`;
-    result.style.color = "#ffffff";
-    // result.style.backgroundColor = "#6fbff7";
-    // inst.style.backgroundColor ="#6fbff7";
-});
 
-hardBtn.addEventListener("click", function() {
+easyBtn.addEventListener("click", function() {
     attempt = 10;
     result.textContent = `You have ${attempt} chances left`;
     result.style.color = "#ffffff";
-    // result.style.backgroundColor = "#6fbff7";
-    // inst.style.backgroundColor ="#6fbff7";
+    result.style.width = "50%";
+    result.style.borderRadius = "10px";
+    alert('Easy Mode Activated');
+    return;
+});
+
+hardBtn.addEventListener("click", function() {
+    attempt = 5;
+    result.textContent = `You have ${attempt} chances left`;
+    result.style.color = "#ffffff";
+    result.style.width = "50%";
+    result.style.borderRadius = "10px";
+    alert('Hard Mode Activated');
+    return;
 });
 
 function refresh(){
@@ -34,75 +39,90 @@ function refresh(){
     }
 }
 
+function disableButton(cmg) {
+    const button = document.getElementById(cmg);
+    button.disabled = true;
+}
+
+play.style.display = "none";
+
 function check() {
-   
     var input = parseInt(guess.value);
 
-    if(attempt === 0){
-        alert("Please Choose the Difficulty 😟");
+    if (isNaN(input) || input < 1 || input > 100) {
+        notifywin.textContent = "Please enter a valid number between 1 and 100 🥲";
+        inst.style.backgroundColor = "#3d90bf";
+        result.textContent = "You have " + attempt + " chances left";
+        notifywin.style.backgroundColor = "#3d90bf";
+        result.style.backgroundColor = "#3d90bf";
+        result.style.padding = "5px";
+        inst.style.color = "#ffffff";
+        result.style.color = "#ffffff";
+        adjustInstSize();
         return;
     }
-
-    else if (isNaN(input) || input < 1 || input > 100) {
-        notifywin.textContent = "Please enter a valid number between 1 and 100 🥲";
-        inst.style.backgroundColor = "#405D72";
-        result.textContent = "You have " + attempt + " chances left";
-        notifywin.style.backgroundColor = "#405D72";
-        inst.style.color = "#ffffff";
-        adjustInstSize();
-        return; 
-    }
-  
-
-   else if (attempt <= 0) {
+    else if (attempt <= 0) {
         notifywin.textContent = "Oooopssss... You lost the game 😭";
         result.textContent = "The Magic Number is: "+ magicNumber;
         notifywin.style.backgroundColor = "#ea431f";
         inst.style.backgroundColor = "#ea431f";
         inst.style.color = "#ffffff";
+        result.style.color = "#ffffff";
+        result.style.backgroundColor = "#ea431f";
+        jsConfetti.addConfetti({emojis: ['😭','😥','😥','😔','😔','😭']});
+        result.style.paddingBottom = "0px";
+        play.style.display = "block";
         adjustInstSize();
+        disableButton("cmg");
         return;
     }
     else if (input === magicNumber) {
         notifywin.textContent = " ";
-        // result.textContent = "You have " + attempt + " chances left";
         result.innerText = "Wowww! You won the game 🎁";
+        result.style.padding = "5px";
+        result.style.width = "80%";
         notifywin.style.backgroundColor = "#53995a";
         inst.style.backgroundColor = "#53995a";
+        result.style.backgroundColor = "#53995a";
+        result.style.color = "#ffffff";
         jsConfetti.addConfetti();
+        play.style.display = "block";
+        disableButton("cmg");
         adjustInstSize();
-        // setTimeout(function() {
-        //     alert("");
-        // }, 1000);
         return;
     }
-
-    else{
+    else {
         attempt--;
         if (attempt <= 0) {
             notifywin.textContent = "Oooopssss... You lost the game 😭";
-            result.textContent = "The Magic Number is:"+ magicNumber;
+            result.textContent = "The Magic Number is: "+ magicNumber;
+            play.style.display = "block";
             notifywin.style.backgroundColor = "#ea431f";
             inst.style.backgroundColor = "#ea431f";
             inst.style.color = "#ffffff";
+            result.style.color = "#ffffff";
+            result.style.backgroundColor = "#ea431f";
+            jsConfetti.addConfetti({emojis: ['😭','😥','😥','😔','😔','😭']});
+            result.style.paddingBottom = "0px";
             adjustInstSize();
+            disableButton("cmg");
             return;
-        } 
-        
-        else{
+        } else {
             notifywin.textContent = input > magicNumber ? "Your guess is greater than the magic number 🥺" : "Your guess is smaller than the magic number 🫣";
             result.textContent = "You have " + attempt + " chances left";
+            result.style.padding = "5px";
             notifywin.style.backgroundColor = "#037E8C";
+            result.style.color = "#ffffff";
             inst.style.backgroundColor = "#037E8C";
+            result.style.backgroundColor = "#037E8C";
             inst.style.color = "#ffffff";
             adjustInstSize();
         }
-        
     }
 }
 
 function adjustInstSize() {
-    inst.style.width = '400px'; // reset the height
-    var width = inst.scrollWidth; // get the scrollHeight which gives the full height of the content
-    inst.style.width = width + 'px'; // set the height dynamically
+    inst.style.width = '400px';
+    var width = inst.scrollWidth;
+    inst.style.width = width + 'px';
 }
